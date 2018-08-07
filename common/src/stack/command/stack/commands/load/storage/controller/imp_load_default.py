f7f7ed8b41a98d6d5ea81a0b32014ab659868bd5
Taylor Sanchez <xTaylorSanchez@gmail.com>
2018-08-07 11:04:07 -0700

    load storage controller updates

--- common/src/stack/command/stack/commands/load/storage/controller/imp_load_default.py
+++ common/src/stack/command/stack/commands/load/storage/controller/imp_load_default.py
@@ -14,7 +14,8 @@ from stack.exception import CommandError
 class Implementation(stack.commands.ApplianceArgumentProcessor,
 	stack.commands.HostArgumentProcessor,
 	stack.commands.NetworkArgumentProcessor,
-	stack.commands.Implementation):	
+	stack.commands.EnvironmentArgumentProcessor,
+	stack.commands.Implementation):
 
 	"""
 	Put storage controller configuration into the database based on
@@ -27,13 +28,13 @@ class Implementation(stack.commands.ApplianceArgumentProcessor,
 		#
 		if slot is None:
 			msg = 'empty value found for "slot" column at line %d' % line
-			CommandError(self.owner, msg)
+			raise CommandError(self.owner, msg)
 		if raid is None:
 			msg = 'empty value found for "raid level" column at line %d' % line
-			CommandError(self.owner, msg)
+			raise CommandError(self.owner, msg)
 		if array is None:
 			msg = 'empty value found for "array id" column at line %d' % line
-			CommandError(self.owner, msg)
+			raise CommandError(self.owner, msg)
 
 		if host not in self.owner.hosts.keys():
 			self.owner.hosts[host] = {}
@@ -52,7 +53,7 @@ class Implementation(stack.commands.ApplianceArgumentProcessor,
 
 		if slot == '*' and raid != 0:
 			msg = 'raid level must be "0" when slot is "*". See line %d' % (line)
-			CommandError(self.owner, msg)
+			raise CommandError(self.owner, msg)
 
 		if 'slot' not in self.owner.hosts[host][array].keys():
 			self.owner.hosts[host][array]['slot'] = []
@@ -69,7 +70,7 @@ class Implementation(stack.commands.ApplianceArgumentProcessor,
 		else:
 			if slot in self.owner.hosts[host][array]['slot']:
 				msg = 'duplicate slot "%s" found in the spreadsheet at line %d' % (slot, line)
-				CommandError(self.owner, msg)
+				raise CommandError(self.owner, msg)
 
 			if raid == 'hotspare':
 				if 'hotspare' not in self.owner.hosts[host][array].keys():
@@ -83,7 +84,7 @@ class Implementation(stack.commands.ApplianceArgumentProcessor,
 
 				if raid != self.owner.hosts[host][array]['raid']:
 					msg = 'RAID level mismatch "%s" found in the spreadsheet at line %d' % (raid, line)
-					CommandError(self.owner, msg)
+					raise CommandError(self.owner, msg)
 
 
 	def run(self, args):
@@ -94,7 +95,6 @@ class Implementation(stack.commands.ApplianceArgumentProcessor,
 		reader = stack.csv.reader(open(filename, 'rU'))
 		header = None
 		line = 0
-
 		name = None
 
 		for row in reader:
@@ -114,7 +114,7 @@ class Implementation(stack.commands.ApplianceArgumentProcessor,
 
 				if len(required) > 0:
 					msg = 'the following required fields are not present in the input file: "%s"' % ', '.join(required)	
-					CommandError(self.owner, msg)
+					raise CommandError(self.owner, msg)
 
 				continue
 
@@ -124,6 +124,7 @@ class Implementation(stack.commands.ApplianceArgumentProcessor,
 			options = None
 			enclosure = None
 			adapter = None
+			scope = None
 
 			for i in range(0, len(row)):
 				field = row[i]
@@ -132,7 +133,8 @@ class Implementation(stack.commands.ApplianceArgumentProcessor,
 
 				if header[i] == 'name':
 					name = field.lower()
-
+				elif header[i] == 'scope':
+					scope = field.lower()
 				elif header[i] == 'slot':
 					if field == '*':
 						slot = field
@@ -141,11 +143,11 @@ class Implementation(stack.commands.ApplianceArgumentProcessor,
 							slot = int(field)
 						except:
 							msg = 'slot "%s" must be an integer' % field
-							CommandError(self.owner, msg)
+							raise CommandError(self.owner, msg)
 
 						if slot < 0:
 							msg = 'slot "%d" must be 0 or greater' % slot
-							CommandError(self.owner, msg)
+							raise CommandError(self.owner, msg)
 
 				elif header[i] == 'raid level':
 					raid = field.lower()
@@ -160,11 +162,11 @@ class Implementation(stack.commands.ApplianceArgumentProcessor,
 							array = int(field)
 						except:
 							msg = 'array "%s" must be an integer' % field
-							CommandError(self.owner, msg)
+							raise CommandError(self.owner, msg)
 
 						if array < 0:
 							msg = 'array "%d" must be 0 or greater' % array
-							CommandError(self.owner, msg)
+							raise CommandError(self.owner, msg)
 
 				elif header[i] == 'options':
 					if field:
@@ -178,33 +180,34 @@ class Implementation(stack.commands.ApplianceArgumentProcessor,
 					if field:
 						adapter = field
 
-			#
 			# the first non-header line must have a host name
-			#
-			if line == 1 and not name:
+			if line == 1 and name is None:
 				msg = 'empty host name found in "name" column'
-				CommandError(self.owner, msg)
-
+				raise CommandError(self.owner, msg)
+			# If scope is global, no name is allowed, coerce to global
+			if scope == 'global':
+				name = 'global'
 			if name in self.appliances or name == 'global':
 				hosts = [ name ]
+			# New logic for scope options
+			elif scope is not None and scope != 'host':
+				hosts = [name]
 			else:
 				hosts = self.getHostnames([ name ])
 
 			if not hosts:
 				msg = 'Cannot find "%s"' % name
-				CommandError(self.owner, msg)
+				raise CommandError(self.owner, msg)
 
 			for host in hosts:
 				self.doit(host, slot, enclosure, adapter,
 					raid, array, options, line)
 
-		#
 		# do final validation
-		#
 		for host in self.owner.hosts.keys():
 			for array in self.owner.hosts[host].keys():
 				if array != 'global' and len(self.owner.hosts[host][array]['slot']) == 0:
 
 					msg = 'hotspare for "%s" for array "%s" is not associated with a disk array' % (host, array)
-					CommandError(self.owner, msg)
+					raise CommandError(self.owner, msg)
 
